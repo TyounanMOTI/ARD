9460d3d530b3ba224f062385bf44aeca92424195
TyounanMOTI <tyounan.moti@gmail.com>
2011-06-03 15:29:43 +0900

    add test for FFTW3

---
+++ ARD_test/test/fftw_test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <fftw3.h>
+
+TEST(FFTWTest, initialization) {
+  fftw_complex* subject = static_cast<fftw_complex*>(fftw_malloc(sizeof(fftw_complex) * 100));
+  subject[0][0] = 4.0;
+  EXPECT_EQ(subject[0][0], 4.0);
+}
