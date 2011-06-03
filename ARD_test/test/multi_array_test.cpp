8eacadf278beafab1923a68c745e1f5b8aa46f82
TyounanMOTI <tyounan.moti@gmail.com>
2011-06-03 18:12:05 +0900

    distilled subject initialization in MultiArrayTest

--- ARD_test/test/multi_array_test.cpp
+++ ARD_test/test/multi_array_test.cpp
@@ -1,19 +1,26 @@
 #include <gtest/gtest.h>
-#include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
 #include <multi_array.h>
 
 using namespace ARD;
 
-TEST(MultiArrayTest, GetSize) {
-  Size size(20, 10);
-  boost::scoped_ptr<MultiArray> subject(new MultiArray(size));
+class MultiArrayTest : public testing::Test
+{
+protected:
+  virtual void SetUp() {
+    size = Size(20, 10);
+    subject = MultiArrayPtr(new MultiArray(size));
+  }
   
+  Size size;
+  typedef boost::shared_ptr<MultiArray> MultiArrayPtr;
+  MultiArrayPtr subject;
+};
+
+TEST_F(MultiArrayTest, GetSize) {
   EXPECT_EQ(subject->GetSize(), size);
 }
 
-TEST(MultiArrayTest, GetArrayElement) {
-  Size size(20, 10);
-  boost::scoped_ptr<MultiArray> subject(new MultiArray(size));
-  
+TEST_F(MultiArrayTest, GetArrayElement) {
   EXPECT_EQ(subject->GetData(0,0), 0.0);
 }
