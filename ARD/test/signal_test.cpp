d2b08df057290f0edee5223cfb75fe0bd50df438
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2011-06-10 09:41:22 +0900

    distill subject initialization of SignalTest

--- ARD/test/signal_test.cpp
+++ ARD/test/signal_test.cpp
@@ -1,14 +1,25 @@
 #include <gtest/gtest.h>
 #include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
 #include <vector>
 #include <signal.h>
 
 using namespace ARD;
 
-TEST(SignalTest, InitializeWithContent) {
-  std::vector<short> content;
-  content.push_back(5);
-  boost::scoped_ptr<Signal> subject(new Signal(content));
+class SignalTest : public testing::Test {
+protected:
+  virtual void SetUp() {
+    content.push_back(5);
+    subject = SignalPointer(new Signal(content));
+  }
+
+  typedef std::vector<short> Content;
+  typedef boost::shared_ptr<Signal> SignalPointer;
+  Content content;
+  SignalPointer subject;
+};
+
+TEST_F(SignalTest, InitializeWithContent) {
   std::vector<short> output = subject->Content();
   EXPECT_EQ(output[0], 5);
 }
