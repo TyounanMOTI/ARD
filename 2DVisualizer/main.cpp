af4b1f27b72441bebeb91e9a33b68cfd62e6f987
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2011-06-25 15:50:14 +0900

    distilled Init and loop

--- 2DVisualizer/main.cpp
+++ 2DVisualizer/main.cpp
@@ -2,17 +2,41 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_main.h>
 
-int main(int argc, char** argv) {
-  SDL_Surface* screen;
-  SDL_Event event;
+SDL_Surface* g_screen;
+
+void Init() {
   SDL_Init(SDL_INIT_VIDEO);
   atexit(SDL_Quit);
-  screen = SDL_SetVideoMode(800, 600, 24, SDL_SWSURFACE);
-  
+  g_screen = SDL_SetVideoMode(800, 600, 24, SDL_SWSURFACE);  
+}
+
+enum LOOP_EXIT_TYPE{
+  CONTINUE = -1,
+  EXIT,
+  ERROR,
+};
+
+// main loop.
+// return: > 0   - error
+//         == 0  - normal exit
+//         == -1 - continue
+int loop(SDL_Event event) {
+  switch (event.type) {
+    case SDL_QUIT: return EXIT;
+    default: break;
+  }
+  return CONTINUE;
+}
+
+int main(int argc, char** argv) {
+  Init();
+
+  SDL_Event event;
+  int status;
   while(SDL_WaitEvent(&event) >= 0) {
-    switch (event.type) {
-      case SDL_QUIT: return 0;
-      default: break;
+    status = loop(event);
+    if (status != CONTINUE) {
+      return status;
     }
   }
   return 0;
