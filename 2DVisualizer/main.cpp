1c38a3c711fa0c809a8480bbb54346685c622df4
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2011-06-25 15:37:58 +0900

    enable to close window

--- 2DVisualizer/main.cpp
+++ 2DVisualizer/main.cpp
@@ -4,9 +4,16 @@
 
 int main(int argc, char** argv) {
   SDL_Surface* screen;
+  SDL_Event event;
   SDL_Init(SDL_INIT_VIDEO);
   atexit(SDL_Quit);
   screen = SDL_SetVideoMode(800, 600, 24, SDL_SWSURFACE);
-  while(1);
+  
+  while(SDL_WaitEvent(&event) >= 0) {
+    switch (event.type) {
+      case SDL_QUIT: return 0;
+      default: break;
+    }
+  }
   return 0;
 }
