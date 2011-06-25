c2fb5117d53117d653485e6f33e94cfc6c17c72f
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2011-06-25 15:22:01 +0900

    show 800x600 Window

--- 2DVisualizer/main.cpp
+++ 2DVisualizer/main.cpp
@@ -1,5 +1,12 @@
+#include <stdlib.h>
+#include <SDL/SDL.h>
 #include <SDL/SDL_main.h>
 
 int main(int argc, char** argv) {
+  SDL_Surface* screen;
+  SDL_Init(SDL_INIT_VIDEO);
+  atexit(SDL_Quit);
+  screen = SDL_SetVideoMode(800, 600, 24, SDL_SWSURFACE);
+  while(1);
   return 0;
 }
