9caa579bd7326205d7b66912f3b7ded2b679b9d7
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2011-07-06 16:57:02 +0900

    implement Scene::set_source by no test

--- ARD/include/scene.h
+++ ARD/include/scene.h
@@ -5,6 +5,7 @@
 #include "signal.h"
 #include "size.h"
 #include "microphone.h"
+#include "source.h"
 
 namespace ARD
 {
@@ -15,10 +16,12 @@ namespace ARD
     MicrophonePointer Update();
     Size size() { return size_; };
     void set_microphone(MicrophonePointer microphone) { microphone_ = microphone; };
+    void set_source(SourcePointer source) { source_ = source; };
 
   private:
     Size size_;
     MicrophonePointer microphone_;
+    SourcePointer source_;
   };
 
   typedef boost::shared_ptr<Scene> ScenePointer;
