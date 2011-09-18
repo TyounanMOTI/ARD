1512e8b83905094b729ef2f5408d11653b688991
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2011-09-18 21:51:44 +0900

    combine fftw_[float|double]_dct_engine.h and use DCT-I

---
+++ ARD/include/fftw_dct_engine.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <boost/shared_ptr.hpp>
+#include <fftw3.h>
+#include "dct_engine.h"
+#include "fftw_array.h"
+
+namespace ARD
+{
+  template <class Precision, class FFTWPlan>
+  class FFTWDCTEngine : public DCTEngine<Precision>
+  {
+  private:
+    typedef boost::shared_ptr<ArrayInterface<Precision> > ArrayInterfacePointer;
+    typedef boost::shared_ptr<FFTWArray<Precision> > FFTWArrayPointer;
+    
+  public:
+    enum TransformDirection {
+      Forward,
+      Backward,
+    };
+
+    FFTWDCTEngine(const Size& size, const TransformDirection direction);
+    const ArrayInterfacePointer input() const { return input_; }
+    const ArrayInterfacePointer Execute();
+
+  private:
+    void Normalize();
+
+    FFTWPlan plan_;
+    FFTWArrayPointer input_;
+    FFTWArrayPointer output_;
+    TransformDirection direction_;
+  };
+
+  template <class Precision, class FFTWPlan>
+  FFTWDCTEngine<Precision, FFTWPlan>::FFTWDCTEngine(const Size& size, const TransformDirection direction) {
+    input_.reset(new FFTWArray<double>(size));
+    output_.reset(new FFTWArray<double>(size));
+    plan_.reset(fftw_plan_r2r_2d(size.height(), size.width(), input_->get(), output_->get(), 
+      (fftw_r2r_kind) FFTW_REDFT00, (fftw_r2r_kind) FFTW_REDFT00, FFTW_ESTIMATE),
+      fftw_destroy_plan);
+    direction_ = direction;
+  }
+
+  template <class Precision, class FFTWPlan>
+  const boost::shared_ptr<ArrayInterface<Precision> > FFTWDCTEngine<Precision, FFTWPlan>::Execute() {
+    fftw_execute(plan_.get());
+    if (direction_ == Forward) {
+      Normalize();
+    }
+    return output_;
+  }
+
+  template <class Precision, class FFTWPlan>
+  void FFTWDCTEngine<Precision, FFTWPlan>::Normalize() {
+    (*output_) /= 4.0*(output_->size().height() - 1)*(output_->size().width() - 1);
+  }
+
+  typedef boost::shared_ptr<fftw_plan_s> FFTWDoublePlan;
+  typedef boost::shared_ptr<fftwf_plan_s> FFTWFloatPlan;
+
+  template <> FFTWDCTEngine<float, FFTWFloatPlan>::FFTWDCTEngine(const Size& size, const TransformDirection direction);
+  template <> const boost::shared_ptr<ArrayInterface<float> > FFTWDCTEngine<float, FFTWFloatPlan>::Execute();
+}
+
--- ARD/include/fftw_dct_engine_factory.h
+++ ARD/include/fftw_dct_engine_factory.h
@@ -1,11 +1,9 @@
-#ifndef FFTW_DCT_ENGINE_FACTORY_H
-#define FFTW_DCT_ENGINE_FACTORY_H
+#pragma once
 
 #include <boost/shared_ptr.hpp>
 #include "dct_engine.h"
 #include "dct_engine_factory.h"
-#include "fftw_double_dct_engine.h"
-#include "fftw_float_dct_engine.h"
+#include "fftw_dct_engine.h"
 
 namespace ARD {
   template <class Precision>
@@ -18,10 +16,16 @@ namespace ARD {
     DCTEnginePointer GenerateForwardEngine(const Size& size);
     DCTEnginePointer GenerateBackwardEngine(const Size& size);
   };
-  
+
+  typedef boost::shared_ptr<fftw_plan_s> FFTWDoublePlan;
+  typedef boost::shared_ptr<fftwf_plan_s> FFTWFloatPlan;
+
+  typedef FFTWDCTEngine<double, FFTWDoublePlan> FFTWDoubleDCTEngine;
+  typedef FFTWDCTEngine<float, FFTWFloatPlan> FFTWFloatDCTEngine;
+
   template <class Precision>
   typename FFTWDCTEngineFactory<Precision>::DCTEnginePointer FFTWDCTEngineFactory<Precision>::GenerateForwardEngine(const Size& size) {
-    return DCTEnginePointer(new FFTWDoubleDCTEngine(size, FFTWDoubleDCTEngine::Forward));    
+    return DCTEnginePointer(new FFTWDoubleDCTEngine(size, FFTWDoubleDCTEngine::Forward));
   }
   
   template <class Precision>
@@ -36,4 +40,3 @@ namespace ARD {
   boost::shared_ptr<DCTEngine<float> > FFTWDCTEngineFactory<float>::GenerateBackwardEngine(const Size& size);
 }
 
-#endif // FFTW_DCT_ENGINE_FACTORY_H
