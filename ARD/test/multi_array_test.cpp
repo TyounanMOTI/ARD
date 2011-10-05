26ad862ea6df5aee3528429c63a72f5648436a7b
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2011-10-05 14:24:41 +0900

    test strong typedefining

--- ARD/test/multi_array_test.cpp
+++ ARD/test/multi_array_test.cpp
@@ -1,6 +1,27 @@
 #include <gtest/gtest.h>
 #include <boost/multi_array.hpp>
+#include <boost/strong_typedef.hpp>
 
 TEST(MultiArrayTest, GetElementType) {
   ::testing::StaticAssertTypeEq<boost::multi_array<int, 3>::element, int>();
+}
+
+TEST(MultiArrayTest, WeakTyping) {
+  typedef float cudaReal;
+  typedef float fftwReal;
+  ::testing::StaticAssertTypeEq<boost::multi_array<cudaReal, 3>, boost::multi_array<fftwReal, 3> >();
+}
+
+TEST(StrongTypedefTest, StrongTypedefining) {
+  BOOST_STRONG_TYPEDEF(float, cudaReal);
+  BOOST_STRONG_TYPEDEF(float, fftwReal);
+  // fails
+  // ::testing::StaticAssertTypeEq<boost::multi_array<cudaReal, 3>, boost::multi_array<fftwReal, 3> >();
+  EXPECT_EQ(sizeof(float), sizeof(cudaReal));
+  EXPECT_EQ(sizeof(cudaReal), sizeof(fftwReal));
+}
+
+TEST(StrongTypedefTest, SizeofDouble) {
+  BOOST_STRONG_TYPEDEF(double, cudaReal);
+  EXPECT_EQ(sizeof(double), sizeof(cudaReal));
 }
