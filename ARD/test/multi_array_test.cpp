c9fe8ece267cbdaf5e398b5c3547c5baedf64342
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2011-10-06 19:21:58 +0900

    we can use vector as index of multi_array

--- ARD/test/multi_array_test.cpp
+++ ARD/test/multi_array_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <boost/multi_array.hpp>
 #include <boost/strong_typedef.hpp>
+#include <boost/assign.hpp>
 
 TEST(MultiArrayTest, GetElementType) {
   ::testing::StaticAssertTypeEq<boost::multi_array<int, 3>::element, int>();
@@ -16,6 +17,17 @@ TEST(MultiArrayTest, WeakTyping) {
   typedef float cudaReal;
   typedef float fftwReal;
   ::testing::StaticAssertTypeEq<boost::multi_array<cudaReal, 3>, boost::multi_array<fftwReal, 3> >();
+}
+
+TEST(MultiArrayTest, IndexAccessingByVector) {
+  using namespace boost::assign;
+
+  typedef boost::multi_array<int, 3> MultiArray;
+  MultiArray subject(boost::extents[20][10][5]);
+  std::vector<MultiArray::index> index;
+  index += 1,2,3;
+  subject(index) = 3;
+  EXPECT_EQ(3, subject(index));
 }
 
 TEST(StrongTypedefTest, StrongTypedefining) {
