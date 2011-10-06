b2aad6d430bf8a3aa2769d46ad6f8751cf9ea1a1
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2011-10-06 18:16:13 +0900

    rename fftwFloatAllocator to FFTWFloatAllocator

--- ARD/include/fftw_float_allocator.h
+++ ARD/include/fftw_float_allocator.h
@@ -7,19 +7,19 @@ namespace ARD {
   BOOST_STRONG_TYPEDEF(float, fftwFloat);
 }
 
-typedef std::allocator<ARD::fftwFloat> fftwFloatAllocator;
+typedef std::allocator<ARD::fftwFloat> FFTWFloatAllocator;
 
 template <>
-fftwFloatAllocator::pointer fftwFloatAllocator::allocate(size_type num) {
+FFTWFloatAllocator::pointer FFTWFloatAllocator::allocate(size_type num) {
   return (pointer)( fftwf_malloc(num * sizeof(ARD::fftwFloat)) );
 }
 
 template <>
-fftwFloatAllocator::pointer fftwFloatAllocator::allocate(fftwFloatAllocator::size_type num, std::allocator<void>::const_pointer hint) {
+FFTWFloatAllocator::pointer FFTWFloatAllocator::allocate(FFTWFloatAllocator::size_type num, std::allocator<void>::const_pointer hint) {
   return allocate(num);
 }
 
 template <>
-void fftwFloatAllocator::deallocate(fftwFloatAllocator::pointer p, fftwFloatAllocator::size_type) {
+void FFTWFloatAllocator::deallocate(FFTWFloatAllocator::pointer p, FFTWFloatAllocator::size_type) {
   fftwf_free(p);
 }
