5b901c82ea8eedaf8ffd44e3505e3312683a1bb8
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2011-10-23 20:41:22 +0900

    declare MultiArray as child of boost::multi_array

--- ARD/include/fftw_multi_array.h
+++ ARD/include/fftw_multi_array.h
@@ -1,8 +1,24 @@
 #pragma once
 
 #include <boost/multi_array.hpp>
-#include <fftw_float_allocator.h>
+#include "fftw_float_allocator.h"
 
 namespace ARD {
-  typedef boost::multi_array<float, 2, FFTWFloatAllocator> FFTWFloat2DArray;
+  template <typename ValueType,
+            std::size_t NumDims,
+            typename Allocator = std::allocator<ValueType> >
+    class MultiArray : public boost::multi_array<ValueType,
+                                                 NumDims,
+                                                 Allocator>
+  {
+    typedef boost::multi_array<ValueType, NumDims, Allocator> super_type;
+
+  public:
+    MultiArray() : super_type() {}
+
+    template <typename ExtentList>
+    explicit MultiArray(const ExtentList& sizes) : super_type(sizes) {}
+  };
+
+  typedef MultiArray<float, 2, FFTWFloatAllocator> FFTWFloat2DArray;
 }
