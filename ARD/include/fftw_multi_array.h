3abe2bcadd73389f6633c76d6962a571e945a0b0
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2011-10-29 12:13:49 +0900

    moved definition of MultiArray to multi_array.h

--- ARD/include/fftw_multi_array.h
+++ ARD/include/fftw_multi_array.h
@@ -3,30 +3,8 @@
 #include <boost/multi_array.hpp>
 #include <array>
 #include "fftw_float_allocator.h"
+#include "multi_array.h"
 
 namespace ARD {
-  template <typename ValueType,
-            std::size_t NumDims,
-            typename Allocator = std::allocator<ValueType> >
-    class MultiArray : public boost::multi_array<ValueType,
-                                                 NumDims,
-                                                 Allocator>
-  {
-    typedef boost::multi_array<ValueType, NumDims, Allocator> super_type;
-
-  public:
-    typedef typename super_type::size_type size_type;
-    typedef boost::array<size_type, NumDims> size_list;
-
-    MultiArray() : super_type() {}
-
-    template <typename ExtentList>
-    explicit MultiArray(const ExtentList& sizes) : super_type(sizes) {}
-
-    size_list shape() {
-      return super_type::extent_list_;
-    }
-  };
-
   typedef MultiArray<float, 2, FFTWFloatAllocator> FFTWFloat2DArray;
 }
--- ARD/include/multi_array.h
+++ ARD/include/multi_array.h
@@ -1,6 +1,29 @@
 #include <boost/multi_array.hpp>
 
 namespace ARD {
+  template <typename ValueType,
+            std::size_t NumDims,
+            typename Allocator = std::allocator<ValueType> >
+    class MultiArray : public boost::multi_array<ValueType,
+                                                 NumDims,
+                                                 Allocator>
+  {
+    typedef boost::multi_array<ValueType, NumDims, Allocator> super_type;
+
+  public:
+    typedef typename super_type::size_type size_type;
+    typedef boost::array<size_type, NumDims> size_list;
+
+    MultiArray() : super_type() {}
+
+    template <typename ExtentList>
+    explicit MultiArray(const ExtentList& sizes) : super_type(sizes) {}
+
+    size_list shape() {
+      return super_type::extent_list_;
+    }
+  };
+
   template <typename T, std::size_t Dim, typename Allocator>
   void Fill(boost::multi_array<T, Dim, Allocator>& target, const T& value) {
     std::fill(target.origin(), target.origin() + target.num_elements(), value);
