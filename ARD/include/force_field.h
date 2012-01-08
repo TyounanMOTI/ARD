6ddf2f596f6980b73c65c86f99dfc38ef604c038
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2012-01-08 15:16:55 +0900

    make ForceField has-a MultiArray

--- ARD/include/force_field.h
+++ ARD/include/force_field.h
@@ -2,9 +2,26 @@
 
 namespace ARD {
   template <class ArrayType>
-  class ForceField : public ArrayType {
+  class ForceField {
   public:
     template <class ExtentList>
-    explicit ForceField(const ExtentList& sizes) : ArrayType(sizes) {};
+    explicit ForceField(const ExtentList& sizes) : _data(ArrayType(sizes)) {};
+
+    typename ArrayType::reference operator[](typename ArrayType::index i) {
+      return _data[i];
+    }
+
+    template <class IndexList>
+    typename ArrayType::element& operator()(const IndexList& indices) {
+      return _data(indices);
+    }
+
+    template <class IndexList>
+    const typename ArrayType::element& operator()(const IndexList& indices) const {
+      return _data(indices);
+    }
+
+  private:
+    ArrayType _data;
   };
 }
