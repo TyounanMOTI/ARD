eb2852ec6cb0223e8c9cfccac140e03cf4b8aa52
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2012-02-05 07:57:00 +0900

    implement 3D space simulation

--- 2DVisualizer/main.cpp
+++ 2DVisualizer/main.cpp
@@ -10,6 +10,7 @@
 #include <ARD.h>
 #include <iostream>
 #include <functional>
+#include <vector>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
@@ -17,21 +18,20 @@ using namespace ARD;
 
 class Scene {
 public:
-  Scene(const int width, const int height, const float dt)
-    : _extents(boost::extents[width][height]),
-      _force_field(_extents),
-      _pressure_field(_extents),
-      _next(new PressureSpectrum<FFTWFloat2DArray>(_extents, _pressure_field)),
-      _current(new PressureSpectrum<FFTWFloat2DArray>(_extents, _pressure_field)),
-      _previous(new PressureSpectrum<FFTWFloat2DArray>(_extents, _pressure_field)),
+  Scene(const std::vector<FFTWFloat3DArray::index>& extents, const float dt)
+    : _shape(extents),
+      _force_field(extents),
+      _pressure_field(extents),
+      _next(new PressureSpectrum<FFTWFloat3DArray>(extents, _pressure_field)),
+      _current(new PressureSpectrum<FFTWFloat3DArray>(extents, _pressure_field)),
+      _previous(new PressureSpectrum<FFTWFloat3DArray>(extents, _pressure_field)),
       _forward_dct_engine(_force_field),
-      _width(width), _height(height),
       _dt(dt), _speed_of_sound(340.0),
-      _omega_square(_extents), _cos(_extents), _current_pressure_spectrum_coeffient(_extents), _force_spectrum_coeffcient(_extents),
+      _omega_square(extents), _cos(extents), _current_pressure_spectrum_coeffient(extents), _force_spectrum_coeffcient(extents),
       _first_frame(true)
   {
     _force_field.Fill(0);
-    _force_field[height/2][width/2] = pow(10,8);
+    _force_field[_shape[0]/2][_shape[1]/2][_shape[2]/2] = pow(10,8);
     _pressure_field.Fill(0);
     InitializeOmegaSquare();
     InitializeCos();
@@ -40,39 +40,47 @@ public:
   }
 
   void InitializeOmegaSquare() {
-    for (int y = 0; y < _height; y++) {
-      for (int x = 0; x < _width; x++) {
-        _omega_square[y][x] = pow(_speed_of_sound, 2) * pow(M_PI, 2) * (pow(x, 2)/pow(_width-1, 2) + pow(y, 2)/pow(_height-1, 2));
+    for (int z = 0; z < _shape[0]; z++) {
+      for (int y = 0; y < _shape[1]; y++) {
+        for (int x = 0; x < _shape[2]; x++) {
+          _omega_square[z][y][x] = pow(_speed_of_sound, 2) * pow(M_PI, 2) * (pow(x, 2)/pow(_shape[2]-1, 2) + pow(y, 2)/pow(_shape[1]-1, 2) + pow(z, 2)/pow(_shape[0]-1, 2));
+        }
       }
     }
   }
 
   void InitializeCos() {
-    for (int y = 0; y < _height; y++) {
-      for (int x = 0; x < _width; x++) {
-        _cos[y][x] = cos(sqrt(_omega_square[y][x])*_dt);
+    for (int z = 0; z < _shape[0]; z++) {
+      for (int y = 0; y < _shape[1]; y++) {
+        for (int x = 0; x < _shape[2]; x++) {
+          _cos[z][y][x] = cos(sqrt(_omega_square[z][y][x])*_dt);
+        }
       }
     }
   }
 
   void InitializeCurrentPressureSpectrumCoefficient() {
-    for (int y = 0; y < _height; y++) {
-      for (int x = 0; x < _width; x++) {
-        _current_pressure_spectrum_coeffient[y][x] = 2.0*_cos[y][x];
+    for (int z = 0; z < _shape[0]; z++) {
+      for (int y = 0; y < _shape[1]; y++) {
+        for (int x = 0; x < _shape[2]; x++) {
+          _current_pressure_spectrum_coeffient[z][y][x] = 2.0*_cos[z][y][x];
+        }
       }
     }
   }
 
   void InitializeForceSpectrumCoefficient() {
-    for (int y = 0; y < _height; y++) {
-      for (int x = 0; x < _width; x++) {
-        _force_spectrum_coeffcient[y][x] = 2.0/_omega_square[y][x]*(1 - _cos[y][x]);
+    for (int z = 0; z < _shape[0]; z++) {
+      for (int y = 0; y < _shape[1]; y++) {
+        for (int x = 0; x < _shape[2]; x++) {
+          _force_spectrum_coeffcient[z][y][x] = 2.0/_omega_square[z][y][x]*(1 - _cos[z][y][x]);
+        }
       }
     }
   }
 
   FFTWFloat2DArray::element* OutputPtr() {
-    return _pressure_field.data();
+    return _pressure_field.data() + _shape[2]*_shape[1]*_shape[0]/2;
   }
 
   void Update() {
@@ -82,13 +90,15 @@ public:
     } else {
       _force_field.Fill(0);
     }
-    const FFTWFloat2DArray& _force_spectrum = _forward_dct_engine.Execute();
-    for (int y = 0; y < _height; y++) {
-      for (int x = 0; x < _width; x++) {
-        _next->_data[y][x] = _current_pressure_spectrum_coeffient[y][x]*_current->_data[y][x] - _previous->_data[y][x] + _force_spectrum_coeffcient[y][x]*_force_spectrum[y][x];
+    const FFTWFloat3DArray& _force_spectrum = _forward_dct_engine.Execute();
+    for (int z = 0; z < _shape[0]; z++) {
+      for (int y = 0; y < _shape[1]; y++) {
+        for (int x = 0; x < _shape[2]; x++) {
+          _next->_data[z][y][x] = _current_pressure_spectrum_coeffient[z][y][x]*_current->_data[z][y][x] - _previous->_data[z][y][x] + _force_spectrum_coeffcient[z][y][x]*_force_spectrum[z][y][x];
+        }
       }
     }
-    _next->_data[0][0] = 0;
+    _next->_data[0][0][0] = 0;
 
     _next->InverseDCT();
     TimeShift();
@@ -100,13 +110,12 @@ public:
   }
 
   //private:
-  decltype(boost::extents[1][1]) _extents;
-  FFTWFloat2DArray _force_field, _pressure_field;
-  std::unique_ptr<PressureSpectrum<FFTWFloat2DArray> > _next, _current, _previous;
-  DCTEngine<FFTWFloat2DArray, Forward> _forward_dct_engine;
-  const int _width, _height;
+  std::vector<FFTWFloat3DArray::index> _shape;
+  FFTWFloat3DArray _force_field, _pressure_field;
+  std::unique_ptr<PressureSpectrum<FFTWFloat3DArray> > _next, _current, _previous;
+  DCTEngine<FFTWFloat3DArray, Forward> _forward_dct_engine;
   const float _dt, _speed_of_sound;
-  FFTWFloat2DArray _omega_square, _cos, _current_pressure_spectrum_coeffient, _force_spectrum_coeffcient;
+  FFTWFloat3DArray _omega_square, _cos, _current_pressure_spectrum_coeffient, _force_spectrum_coeffcient;
   bool _first_frame;
 };
 
@@ -117,17 +126,24 @@ void Update(void*) {
   std::cout << "updated" << std::endl;
 }
 
-int main(void) {
-  const int width = 256;
-  const int height = 256;
-  scene.reset(new Scene(width, height, 0.001));
-  for (int i = 0; i < 100; i++) {
+int main(int argc, char** argv) {
+  int depth, height, width;
+  if (argc < 3) {
+    depth = 64;
+    height = 64;
+    width = 64;
+  } else {
+    depth = atoi(argv[1]);
+    height = atoi(argv[2]);
+    width = atoi(argv[3]);
+  }
+  scene.reset(new Scene(std::vector<FFTWFloat3DArray::index>({depth, height, width}), 0.001));
+  for (int i = 0; i < 10; i++) {
     scene->Update();
   }
 
   vtkSmartPointer<vtkImageImport> imageImport = vtkSmartPointer<vtkImageImport>::New();
-  //  imageImport->SetImportVoidPointer(const_cast<FFTWFloat2DArray::element*>(scene->_next->_data.data()));
-  imageImport->SetImportVoidPointer(const_cast<FFTWFloat2DArray::element*>(scene->OutputPtr()));
+  imageImport->SetImportVoidPointer(const_cast<FFTWFloat3DArray::element*>(scene->OutputPtr()));
   imageImport->SetDataExtent(0,width-1,0,height-1,0,0);
   imageImport->SetWholeExtent(0,width-1,0,height-1,0,0);
   imageImport->SetDataScalarTypeToFloat();
