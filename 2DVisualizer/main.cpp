b44e9e954b4cf1412f9490fbe1c9678b927fae91
Hirotoshi YOSHITAKA <tyounan.moti@gmail.com>
2012-07-22 06:17:14 +0900

    implement basic event wait loop

--- 2DVisualizer/main.cpp
+++ 2DVisualizer/main.cpp
@@ -1,5 +1,32 @@
 #include <SDL.h>
+#include <iostream>
+
+void Init() {
+  if (SDL_Init(SDL_INIT_VIDEO) == -1) {
+    std::cerr << "SDL: video init failed" << std::endl;
+  }
+  SDL_SetVideoMode(800, 600, 32, SDL_SWSURFACE);
+}
+
+void Quit() {
+  SDL_Quit();
+  exit(0);
+}
 
 int SDL_main(int argc, char **argv) {
+  Init();
+
+  SDL_Event event;
+  while(SDL_WaitEvent(&event)) {
+    switch(event.type) {
+    case SDL_QUIT:
+      Quit();
+      break;
+    default:
+      break;
+    }
+  }
+  Quit();
+
   return 0;
 }
